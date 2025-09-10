13f200e8e58aeead6a4a25e9dfd61735c602b786
shuru-sagarj <sagar.jadhav@shurutech.com>
2025-09-10 12:35:47 +0530

    patch: removed auth0 login w/ expo-auth-session

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,10 @@
-import { useCallback, useState } from 'react';
-import {
-    ActivityIndicator,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View
-} from 'react-native';
+import { useCallback, useState } from "react";
+import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-import {Colors} from '@/constants/Colors';
-import {expoSecureStorage} from '@/lib/expoSecureStorage';
-import {setSecureItem, StorageKeys} from '@/lib/storage';
-import * as AuthSession from 'expo-auth-session';
-import { useAuth0 } from 'react-native-auth0';
+import { Colors } from "@/constants/Colors";
+import { useAuth0 } from "react-native-auth0";
 
-import {
-    ANDROID_PACKAGE,
-    AUTHZERO_CLIENT_ID,
-    AUTHZERO_DOMAIN,
-    DUI_CONNECTION_NAME,
-    IOS_BUNDLE,
-    SCHEME,
-} from '@/constants';
+import { DUI_CONNECTION_NAME, SCHEME } from "@/constants";
 
 export default function Login() {
     const [loading, setLoading] = useState(false);
@@ -30,132 +14,42 @@ export default function Login() {
     // Authorization via react-native-oauth
     const login = useCallback(async () => {
         try {
-            const resp = await authorize({ connection: DUI_CONNECTION_NAME, redirectUrl: `${SCHEME}://auth` });
-            console.log('RNAUTH0 Response', resp);
-        } catch (error) {
-            console.log('RNAUTH0 error', error);
-        }
-    }, []);
-
-    // Authorization via expo-auth-session
-    const signinViaOAuth2 = useCallback(async () => {
-        // 0) Ensuring env vars are set
-        if (
-            !AUTHZERO_DOMAIN ||
-            !SCHEME ||
-            !ANDROID_PACKAGE ||
-            !IOS_BUNDLE ||
-            !AUTHZERO_CLIENT_ID ||
-            !DUI_CONNECTION_NAME
-        ) {
-            console.error('Missing one or more required environment variables.');
-            return;
-        }
-
-        setLoading(true);
-        console.log('Auth0 OAuth2 flow started');
-
-        try {
-            // 1) Discovery
-            const discovery = {
-                authorizationEndpoint: `https://${AUTHZERO_DOMAIN}/authorize`,
-                tokenEndpoint: `https://${AUTHZERO_DOMAIN}/oauth/token`,
-            };
-
-            console.log('Auth0 discovery:', discovery);
-
-            // 2) Platform redirecttion
-            const redirectUri = AuthSession.makeRedirectUri({
-                scheme: SCHEME,
-                path: 'auth'
+            setLoading(true);
+            const resp = await authorize({
+                connection: DUI_CONNECTION_NAME,
+                redirectUrl: `${SCHEME}://auth`,
             });
-
-            console.log('Auth0 redirect URI:', redirectUri);
-
-            /**
-             * 3) Building a new request (PKCE is on by default for responseType 'code')
-             * clientSecret will not be needed since we're using PKCE
-             */
-            const request = new AuthSession.AuthRequest({
-                clientId: AUTHZERO_CLIENT_ID,
-                redirectUri,
-                responseType: 'code',
-                //scopes: ['openid', 'profile', 'email', 'offline_access'],
-                extraParams: { connection: DUI_CONNECTION_NAME },
-                // usePKCE: true, // optional - defaults to true for 'code'
-            });
-
-            const requestConfig = await request.getAuthRequestConfigAsync();
-
-            console.log('Auth0 auth config:', requestConfig);
-
-            // 4) Opening browser for Universal Login
-            const result = await request.promptAsync(discovery);
-
-            if (result.type !== 'success' || !result.params.code) {
-                console.log('Auth0 login cancelled/failed:', result);
-                setLoading(false);
-                return;
-            }
-            else {
-                console.log('Auth0 login success, code:', result.params.code);
-            }
-
-            // 5) Exchanging code for tokens (no client secret)
-            const tokenResponse = await AuthSession.exchangeCodeAsync(
-                {
-                    clientId: requestConfig.clientId,
-                    code: result.params.code,
-                    redirectUri: requestConfig.redirectUri,
-                    extraParams: { code_verifier: request.codeVerifier! },
-                },
-                discovery
-            );
-
-            // tokenResponse.accessToken, tokenResponse.idToken, tokenResponse.refreshToken?
-            console.log('Auth0 tokens:', tokenResponse);
-            if (tokenResponse.accessToken) {
-                await setSecureItem(expoSecureStorage, StorageKeys.ID_TOKEN, tokenResponse.accessToken);
-                console.log('ID token saved to secure storage');
-            }
-        } catch (error) {
-            console.error('Error during Auth0 OAuth2 flow:', error);
-        } finally {
+            console.log("RNAUTH0 Response", resp);
             setLoading(false);
-
-            console.log('Auth0 OAuth2 flow ended');
+        } catch (error) {
+            setLoading(false);
+            console.log("RNAUTH0 error", error);
         }
     }, []);
 
     if (isLoading) {
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>SDK is Loading</Text>
             </View>
-        )
+        );
     }
-
-
 
     return (
         <View style={styles.container}>
-            {/* Authorize via expo-auth-session */}
-            <TouchableOpacity disabled={loading} onPress={signinViaOAuth2} style={styles.signIn}>
-                <Text style={styles.signInText}>OAuth2 Sign-In</Text>
-            </TouchableOpacity>
-            {/* Authorize via expo-auth-session */}
-
             {/* Authorize via react-native-auth0 */}
-            {!user && <TouchableOpacity disabled={loading} onPress={login} style={styles.signIn}>
-                <Text style={styles.signInText}>Auth0 lib sign in</Text>
-            </TouchableOpacity>}
+            {!user && (
+                <TouchableOpacity disabled={loading} onPress={login} style={styles.signIn}>
+                    <Text style={styles.signInText}>Auth0 sign in</Text>
+                </TouchableOpacity>
+            )}
             {user && <Text>Logged in as {user.name}</Text>}
             {error && <Text>{error.message}</Text>}
             {/* Authorize via react-native-auth0 */}
 
             {loading && (
                 <ActivityIndicator
-                    size="large"
+                    size='large'
                     color={Colors.light.tint}
                     style={styles.loadingIndicator}
                 />
@@ -166,13 +60,13 @@ export default function Login() {
 
 const styles = StyleSheet.create({
     loadingIndicator: {
-        position: 'absolute',
-        bottom: '40%',
-        alignSelf: 'center',
+        position: "absolute",
+        bottom: "40%",
+        alignSelf: "center",
     },
     container: {
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: "center",
+        justifyContent: "center",
         backgroundColor: Colors.dark.background,
         flex: 1,
         padding: 40,
@@ -190,21 +84,21 @@ const styles = StyleSheet.create({
     inputsContainer: {
         gap: 16,
         paddingHorizontal: 32,
-        width: '100%',
+        width: "100%",
     },
     passwordWrapper: {
-        flexDirection: 'row',
-        alignItems: 'center',
+        flexDirection: "row",
+        alignItems: "center",
     },
     signIn: {
-        backgroundColor: '#2112c9',
+        backgroundColor: "#2112c9",
         borderRadius: 20,
         paddingVertical: 12,
         paddingHorizontal: 20,
         marginTop: 24,
     },
     signInText: {
-        color: '#fff',
-        fontWeight: '700',
+        color: "#fff",
+        fontWeight: "700",
     },
 });
